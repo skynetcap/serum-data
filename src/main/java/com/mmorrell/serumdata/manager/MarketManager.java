a8fea4d37bae439fa592e501ad09f2cc6539daf3
skynetcap <100323448+skynetcap@users.noreply.github.com>
2022-06-05 19:24:02 -0700

    Change cache to use a Map of lists

--- src/main/java/com/mmorrell/serumdata/manager/MarketManager.java
+++ src/main/java/com/mmorrell/serumdata/manager/MarketManager.java
@@ -9,30 +9,31 @@ import org.p2p.solanaj.rpc.types.ProgramAccount;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 public class MarketManager {
 
-    private List<Market> marketCache = new ArrayList<>();
+    private final Map<String, List<Market>> marketMapCache = new HashMap<>();
 
     public MarketManager() {
         updateMarkets();
     }
 
-    public void setMarketCache(final List<Market> markets) {
-        this.marketCache = markets;
-    }
-
     public List<Market> getMarketCache() {
-        return marketCache;
+        return marketMapCache.values().stream()
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
     }
 
     /**
      * Update marketCache with the latest markets
      */
     public void updateMarkets() {
-        marketCache.clear();
+        marketMapCache.clear();
         RpcClient client = new RpcClient("https://ssc-dao.genesysgo.net/");
         List<ProgramAccount> programAccounts = null;
         try {
@@ -52,7 +53,14 @@ public class MarketManager {
 
         for(ProgramAccount programAccount : programAccounts) {
             Market market = Market.readMarket(programAccount.getAccount().getDecodedData());
-            marketCache.add(market);
+
+            // Get list of existing markets for this base mint. otherwise create a new list and put it there.
+            List<Market> existingMarketList = marketMapCache.getOrDefault(market.getBaseMint().toBase58(), new ArrayList<>());
+            existingMarketList.add(market);
+
+            if (existingMarketList.size() == 1) {
+                marketMapCache.put(market.getBaseMint().toBase58(), existingMarketList);
+            }
         }
 
         System.out.println("Cached markets.");
