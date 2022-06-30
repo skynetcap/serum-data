34dd41562719248df857b9eb0be03c3fefba3c9f
skynetcap <100323448+skynetcap@users.noreply.github.com>
2022-06-30 12:42:19 -0700

    Clean up imports

--- src/main/java/com/mmorrell/serumdata/controller/ApiController.java
+++ src/main/java/com/mmorrell/serumdata/controller/ApiController.java
@@ -10,14 +10,12 @@ import com.mmorrell.serumdata.util.MarketUtil;
 import com.mmorrell.serumdata.util.RpcUtil;
 import org.p2p.solanaj.core.PublicKey;
 import org.p2p.solanaj.rpc.RpcClient;
-import org.p2p.solanaj.rpc.RpcException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @RestController
 public class ApiController {
