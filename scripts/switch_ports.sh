45a9fa5ed5dbab123f22cb0c3f127620914dd152
skynetcap <100323448+skynetcap@users.noreply.github.com>
2022-07-07 14:09:33 -0700

    Update NGINX script

--- scripts/switch_ports.sh
+++ scripts/switch_ports.sh
@@ -1,3 +1,4 @@
 sudo sed -i 's/8081/HOLDER/g' /etc/nginx/sites-available/openserum.io
 sudo sed -i 's/8080/8081/g' /etc/nginx/sites-available/openserum.io
 sudo sed -i 's/HOLDER/8080/g' /etc/nginx/sites-available/openserum.io
+sudo systemctl reload nginx
