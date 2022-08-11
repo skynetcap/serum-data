2b2017b99210eafea57e6b865afa77b5338e9e7b
skynetcap <100323448+skynetcap@users.noreply.github.com>
2022-08-10 20:34:05 -0700

    [SRM-129] Add constructors to AccountInfoRow

--- src/main/java/com/mmorrell/serumdata/client/AccountInfoRow.java
+++ src/main/java/com/mmorrell/serumdata/client/AccountInfoRow.java
@@ -3,11 +3,15 @@ package com.mmorrell.serumdata.client;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.mmorrell.serumdata.util.Base64Serializer;
 import com.mmorrell.serumdata.util.PublicKeyByteSerializer;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountInfoRow {
 
     @JsonSerialize(using = PublicKeyByteSerializer.class)
